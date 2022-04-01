4290149f1c4e0c1e8127bbf93fac3a523f129818
jamesjoel <jamesjoelj@gmail.com>
2022-04-01 14:15:45 +0530

    node day 2

---
+++ javascript/array.js
@@ -0,0 +1,10 @@
+var a = [10, 20, 100, 85.71, true, "indore"];
+
+// console.log(a);
+// console.log(a[5]);
+
+// console.log(a[1]+a[2])
+
+
+
+
---
+++ javascript/object.js
@@ -0,0 +1,3 @@
+var data = { a : 100, b : 500, c : 870 };
+
+console.log(data.c);
