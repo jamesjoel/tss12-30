4a90b812bcc6b0c5eb8ec27d4083328fe6f30391
jamesjoel <jamesjoelj@gmail.com>
2022-04-07 14:10:38 +0530

    function in Js

---
+++ javascript/foreach.js
@@ -0,0 +1,6 @@
+var arr = ["red", "green", "blue", "yellow", "pink", "black"];
+
+arr.forEach(function(x, y){
+    console.log(x, y);
+    
+})
---
+++ javascript/fun1.js
@@ -0,0 +1,16 @@
+function demo(a)
+{
+    // var a = 100;
+    var b = 5;
+    var c = 2;
+    
+    var x = a*b;
+    var y = x/c;
+    var z = y+200;
+
+    console.log(y);
+}
+
+demo(200);
+demo(500);
+demo(750);
