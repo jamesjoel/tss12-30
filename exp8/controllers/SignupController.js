fed5c3cb34a193b65e7198bfb669862729c92409
jamesjoel <jamesjoelj@gmail.com>
2022-04-21 18:36:02 +0530

    delete in express

--- exp8/controllers/SignupController.js
+++ exp8/controllers/SignupController.js
@@ -56,4 +56,44 @@ routes.get("/list", (req, res)=>{
     
 })
 
+
+
+
+routes.get("/more/:a", (req, res)=>{
+    // console.log(req.params.a);
+    var id = req.params.a; // 625ebaff1ed4755512095582
+    var objid = mongodb.ObjectId(id);// ObjectId("625ebaff1ed4755512095582")
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5");
+        db.collection("student").find({ _id : objid }).toArray((err, result)=>{
+            // console.log(result);
+            var obj = { result : result[0] };
+            res.render("signup/more", obj);
+        })
+    })
+})
+
+
+routes.get("/delete/:a", (req, res)=>{
+    var id = req.params.a;
+    var objid = mongodb.ObjectId(id);
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        var db = con.db("tss5");
+        db.collection("student").deleteMany({ _id : objid }, (err)=>{
+            res.redirect("/signup/list");
+        })
+    })
+
+})
+
+
+
+
+routes.get("/demo/:x/:y/:z", (req, res)=>{
+    console.log("-------------");
+    console.log(req.params.x);
+    console.log(req.params.y);
+    console.log(req.params.z);
+})
+
 module.exports = routes;
